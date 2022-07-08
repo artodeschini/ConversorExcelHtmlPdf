c9f4a4b67ba4cd4f73164c265a4f577e685f4c6e
C1319689 Artur Todeschini Crestani <artur.crestani@getronics.com>
2022-07-08 19:33:30 -0300

    adicionado leitura e escrita  de alteracao de json

---
+++ leitorJson.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+// exemplo de leitura de arquivos do tipo json
+fs.readFile('./artur.json', {encoding: 'utf-8'}, (err, dados) => {
+
+    if (err) {
+        console.log(`Ocorreu uma falha ao ler o arquivo detalhes >>> ${err}`);
+    } else {
+        
+        console.log(`conteudo original ${dados}`);
+        
+        //JSON.parse converte conteudo em texto para json
+        var content = JSON.parse(dados);
+        
+        //alterando o conteudo lido
+        content.sobrenome += ' Crestani' ;
+        console.log(content);
+
+        //salvando alteracoes no arquivo
+        fs.writeFile(
+            './artur.json', // arquivo a ser escrito
+            JSON.stringify(content), (err) => { /// transforma json em texto para ser escrito no arquivo
+                if (err) {
+                    console.log(`Erro ao alterar o arquivo detalhes >>> ${err}`);
+                }
+            }
+        ); 
+    }
+});
+
+//para executar use node lerArquivo.js
