cdc8b0d84f4ac4811956b8b21c9e61e0c5073823
C1319689 Artur Todeschini Crestani <artur.crestani@getronics.com>
2022-07-08 19:02:18 -0300

    exemplo de leitura de arquivos texto com nodejs

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ConversorExcelHtmlPdf
+# Conversor Excel para Html e PDF
+
+## requisitos
+
+* javascript
+* nodejs
+
---
+++ lerArquivo.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// exemplo de leitura de arquivos do tipo texto
+fs.readFile('./my_file.txt', {encoding: 'utf-8'}, (err, dados) => {
+
+    if (err) {
+        console.log(`Ocorreu uma falha ao ler o arquivo detalhes >>> ${err}`);
+    } else {
+        console.log(dados);
+    }
+});
+
+//para executar use node lerArquivo.js
